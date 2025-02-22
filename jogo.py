a743efb89f83817a6fdeecb03791cffdce6910aa
Bueno06022024 <163076914+Bueno06022024@users.noreply.github.com>
2025-02-21 22:21:24 -0300

    jogo

---
+++ jogo.py
@@ -0,0 +1,15 @@
+print("**********")
+print("bem vindo ao jogo de adivinhaçao")
+print("**********")
+
+numero_secreto = 77
+
+chute_9str = input("digite o seu numero:")
+print ("voce digitou:", chute_str)
+chute = int (chute_str)
+
+if (numero_secreto == chute):
+    print ("voce acertou!")
+else:
+    print("voce acertou!" )
+print ("fim do jogo")
