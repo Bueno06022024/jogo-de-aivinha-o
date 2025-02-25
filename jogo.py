67440ea4bae72333847b803df87e9e6a9a9c332c
Bueno06022024 <163076914+Bueno06022024@users.noreply.github.com>
2025-02-24 22:29:15 -0300

    jogo

--- jogo.py
+++ jogo.py
@@ -3,13 +3,26 @@ print("bem vindo ao jogo de adivinhaçao")
 print("**********")
 
 numero_secreto = 77
+total_de_tentativas = 3
+rodada=1
+while (rodada <= total_de_tentativas):
+    print("tentativas {}".format(rodada,total_de_tentativas))
 
-chute_9str = input("digite o seu numero:")
-print ("voce digitou:", chute_str)
+chute_str = input("digite o seu numero:")
+print ("voce digitou:", chute_str) 
 chute = int (chute_str)
 
-if (numero_secreto == chute):
-    print ("voce acertou!")
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute <numero_secreto
+
+if (acertou):
+    print("Parabéns! voce acertou!")
 else:
-    print("voce acertou!" )
-print ("fim do jogo")
+    if(maior):
+        print("o seu chute foi maior do que o numero secreto!")
+    elif(menor)
+       print("o seu chute foi menor do que o numero secreto!")
+
+    rodada = rodada + 1
+print("fim de jogo")
